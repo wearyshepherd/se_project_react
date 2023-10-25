15e0549dd5b7afd017418a7f28d4401b536a994f
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-10-25 02:32:21 -0700

    Update ItemCard.js

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,16 +1,24 @@
-import "./ItemCard.css";
+import "./ModalWithForm.css";
 
-const ItemCard = ({ key, item, onSelectCard }) => {
+const ModalWithForm = ({ children, buttonText, title, onClose, modalName }) => {
   return (
-    <li className="itemCard">
-      <h3 className="itemCard__name">{item.name}</h3>
-      <img
-        src={item.link}
-        className="itemCard__image"
-        onClick={() => onSelectCard(item)}
-      />
-    </li>
+    <div className={`modal modal_type_${modalName}`}>
+      <div className="modal__content">
+        <button className="modal__close" onClick={onClose}></button>
+        <h3 className="modal__title">{title}</h3>
+        <form className="form">
+          <fieldset className="modal__fieldset">
+            {children}
+            <div className="modal__button-container">
+            <button className="modal__submit-button" type="submit">
+              {buttonText}
+            </button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </div>
   );
 };
 
-export default ItemCard;
+export default ModalWithForm;
