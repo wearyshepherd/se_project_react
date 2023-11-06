4e90cba7cb5091257c86822c3578048f0297b467
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-11-05 19:37:58 -0800

    Update ModalWithForm.js

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./ModalWithForm.css";
+import "./ModalWithForm.css"; // Import your ModalWithForm styles
+import "./YourModalStyles.css"; // Import the provided modal styles
 
 const ModalWithForm = ({ children, buttonText, title, onClose, modalName }) => {
   return (
@@ -8,14 +9,12 @@ const ModalWithForm = ({ children, buttonText, title, onClose, modalName }) => {
         <button className="modal__close" onClick={onClose}></button>
         <h3 className="modal__title">{title}</h3>
         <form className="form">
-          <fieldset className="modal__fieldset">
-            {children}
-            <div className="modal__button-container">
-              <button className="modal__submit-button" type="submit">
-                {buttonText}
-              </button>
-            </div>
-          </fieldset>
+          <fieldset className="modal__fieldset">{children}</fieldset>
+          <div className="modal__button-container">
+            <button className="modal__submit-button" type="submit">
+              {buttonText}
+            </button>
+          </div>
         </form>
       </div>
     </div>
@@ -23,6 +22,3 @@ const ModalWithForm = ({ children, buttonText, title, onClose, modalName }) => {
 };
 
 export default ModalWithForm;
-
-
-
