d81923b23187a806aeb6f4dc117db3b72dc28640
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-11-07 14:40:22 -0800

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,12 @@
+import React, { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import ItemModal from "../ItemModal/ItemModal";
-import { useEffect, useState } from "react";
 import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 import {
   getForecastWeather,
-  parseCityData,
   parseWeatherData,
 } from "../../utils/weatherApi.js";
 import "./App.css";
@@ -17,7 +16,6 @@ function App() {
   const [activeModal, setActiveModal] = useState("");
   const [selectedCard, setSelectedCard] = useState({});
   const [temp, setTemp] = useState(0);
-  const [city, setCity] = useState("");
   const [currentTemperatureUnit, setCurrentTemperatureUnit] = useState("F");
 
   const handleCreateModal = () => {
@@ -34,9 +32,7 @@ function App() {
   };
 
   const handleToggleSwitchChange = () => {
-    currentTemperatureUnit === "F"
-      ? setCurrentTemperatureUnit("C")
-      : setCurrentTemperatureUnit("F");
+    setCurrentTemperatureUnit((prevUnit) => (prevUnit === "F" ? "C" : "F"));
   };
 
   useEffect(() => {
@@ -54,17 +50,16 @@ function App() {
       <CurrentTemperatureUnitContext.Provider
         value={{ currentTemperatureUnit, handleToggleSwitchChange }}
       >
-        <Header onCreateModal={handleCreateModal} weatherCity={city} />
+        <Header onCreateModal={handleCreateModal} />
         <Main weatherTemp={temp} onSelectCard={handleSelectedCard} />
         <Footer />
         {activeModal === "create" && (
           <ModalWithForm
-            title="New Garmnet"
-            buttonText="Add garment"
+            title="New Garment"
+            buttonText="Add Garment"
             onClose={handleCloseModal}
           >
             <label className="modal__label">Name</label>
-
             <input
               className="modal__input modal__input_type_text"
               type="text"
@@ -84,43 +79,37 @@ function App() {
               placeholder="Image URL"
             />
             <label className="modal__label">Select the weather type:</label>
-            <div>
-              <div className="modal__radio-container">
-                <input
-                  className="modal__input_radio"
-                  type="radio"
-                  name="weatherType"
-                  id="Hot"
-                  value="hot"
-                />
-                <label className="modal__label_radio" htmlFor="Hot">
-                  Hot
-                </label>
-              </div>
-              <div className="modal__radio-container">
-                <input
-                  className="modal__input_radio"
-                  type="radio"
-                  name="weatherType"
-                  id="Warm"
-                  value="warm"
-                />
-                <label className="modal__label_radio" htmlFor="Warm">
-                  Warm
-                </label>
-              </div>
-              <div className="modal__radio-container">
-                <input
-                  className=" modal__input_radio"
-                  type="radio"
-                  name="weatherType"
-                  id="Cold"
-                  value="cold"
-                />
-                <label className=" modal__label_radio" htmlFor="Cold">
-                  Cold
-                </label>
-              </div>
+            <div className="modal__radio-container">
+              <input
+                className="modal__input_radio"
+                type="radio"
+                name="weatherType"
+                id="Hot"
+                value="hot"
+              />
+              <label className="modal__label_radio" htmlFor="Hot">
+                Hot
+              </label>
+              <input
+                className="modal__input_radio"
+                type="radio"
+                name="weatherType"
+                id="Warm"
+                value="warm"
+              />
+              <label className="modal__label_radio" htmlFor="Warm">
+                Warm
+              </label>
+              <input
+                className="modal__input_radio"
+                type="radio"
+                name="weatherType"
+                id="Cold"
+                value="cold"
+              />
+              <label className="modal__label_radio" htmlFor="Cold">
+                Cold
+              </label>
             </div>
           </ModalWithForm>
         )}
