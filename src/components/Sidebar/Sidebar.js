fc790c8dbe86b3f3d5c2e50bdb2f93e18217aeca
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-11-11 17:43:45 -0800

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,17 +1,17 @@
-import avatarImage from '../../images/avatar.svg'
+import avatarImage from './avatar.svg';  // Update the path accordingly
 import { Link } from "react-router-dom";
 import './Sidebar.css';
 
 const Sidebar = () => {
     return (
         <>
-      <div className="sidebar__avatar-image">
-        <img src={avatarImage} alt="profile picture" />
-      </div>
-      <div className='sidebar__profile-info'>
-            Terrence Tegegne
-      </div>
-    </>
+            <div className="sidebar__avatar-image">
+                <img src={avatarImage} alt="profile picture" />
+            </div>
+            <div className='sidebar__profile-info'>
+                Terrence Tegegne
+            </div>
+        </>
     )
 }
 
