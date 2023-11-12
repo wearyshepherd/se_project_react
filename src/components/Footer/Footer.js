4ec74d03d6e2d10456ceb39a29be2e65e1302033
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-11-12 14:30:14 -0800

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>Developed by TrippleTen Student Carlos Chavez</div>
+      <div>2023</div>
+    </footer>
+  );
+};
+export default Footer;
