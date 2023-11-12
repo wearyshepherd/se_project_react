4bce3298aa80990ce5544eb644f47c5a6d23de2c
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-11-11 17:19:20 -0800

    Delete src directory

--- src/contexts/CurrentTemperatureUnitContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const CurrentTemperatureUnitContext = createContext({
-  CurrentTemperatureUnit: "",
-  handleToggleSwitchChange: () => {},
-});
-
-export { CurrentTemperatureUnitContext };
