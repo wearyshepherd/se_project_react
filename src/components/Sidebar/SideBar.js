5045e5653100772912233221cee60575603b93da
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-11-13 20:23:20 -0800

    Add files via upload

---
+++ src/components/Sidebar/SideBar.js
@@ -0,0 +1,15 @@
+import avatarImage from "../../images/avatar.svg";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="sidebar__avatar-image">
+        <img src={avatarImage} alt="profile picture" />
+      </div>
+      <div className="sidebar__profile-info">Terrence Tegegne</div>
+    </>
+  );
+};
+
+export default SideBar;
