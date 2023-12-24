d38e1368bfff615b0e32e012fe3b8a6295419678
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2023-12-23 23:25:17 -0800

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,35 +1,88 @@
-// api.js
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "deployed-backend-url"
+    : "http://localhost:3001";
 
-const processServerResponse = (res) => {
-  if (!res.ok) {
-    return Promise.reject(`Error: ${res.status}`);
+export const checkResponse = (res) => {
+  if (res.ok) {
+    return res.json().catch(() => ({}));
+  } else {
+    return res
+      .json()
+      .then((error) => Promise.reject(`Error: ${res.status}, ${error.message}`))
+      .catch(() => Promise.reject(`Error: ${res.status}`));
   }
-  return res.json();
 };
 
-const getCards = () => {
-  return fetch(`${baseUrl}/items`).then(processServerResponse);
-};
-
-const postCard = ({ name, imageUrl, weather }) => {
+export const addItem = ({ name, imageUrl, weather }) => {
+  console.log("Request Payload:", { name, imageUrl, weather });
   return fetch(`${baseUrl}/items`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
     },
-    body: JSON.stringify({
-      name: name,
-      imageUrl: imageUrl,
-      weather: weather,
+    body: JSON.stringify({ name, imageUrl, weather }),
+  })
+    .then((res) => {
+      console.log("Response from addItem:", res);
+      return checkResponse(res);
     })
-  }).then(processServerResponse);
-}
-
-const deleteCard = (id) => {
-  return fetch(`${baseUrl}/items/${id}`, {
-    method: 'DELETE',
-  }).then(processServerResponse);
+    .then((data) => {
+      console.log("Data from addItem:", data);
+      return data;
+    });
 };
 
-export { getCards, postCard, deleteCard, processServerResponse };
+export const getItems = () => {
+  return fetch(`${baseUrl}/items`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(checkResponse);
+};
+
+export const deleteItem = (selectedCard) => {
+  return fetch(`${baseUrl}/items/${selectedCard._id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+    },
+  }).then(checkResponse);
+};
+
+export const addCardLike = (itemId) => {
+  return fetch(`${baseUrl}/items/${itemId}/likes`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed to add like");
+      }
+      return res.json();
+    })
+    .then((data) => data);
+};
+
+export const removeCardLike = (itemId) => {
+  return fetch(`${baseUrl}/items/${itemId}/likes`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed to remove like");
+      }
+      return res.json();
+    })
+    .then((data) => data);
+};
