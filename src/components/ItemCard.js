3c1b7a6536da0a789feaab1066bc42cad3e459bc
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2024-01-15 19:43:37 -0800

    Update ItemCard.js

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 import React, { useContext, useEffect, useState } from "react";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import "../blocks/ItemCard.css";
