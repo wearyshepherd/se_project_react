d868764afa91b5668ca66699f61d9c074e476568
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2024-01-16 21:18:22 -0800

    Add files via upload

---
+++ src/utils/auth.js
@@ -0,0 +1,49 @@
+import { checkResponse } from "./api";
+
+const authBaseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://se-project-react.pages.dev/"
+    : "http://localhost:3001";
+
+export const registerUser = ({ email, password, name, avatar }) => {
+  return fetch(`${authBaseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, name, avatar }),
+  }).then(checkResponse);
+};
+
+export const loginUser = ({ email, password }) => {
+  return fetch(`${authBaseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
+};
+
+export const getUserInfo = (token) => {
+  return fetch(`${authBaseUrl}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(checkResponse);
+};
+
+export const editProfile = (name, avatar, token) => {
+  return fetch(`${authBaseUrl}/users/me`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ name, avatar }),
+  }).then(checkResponse);
+};
