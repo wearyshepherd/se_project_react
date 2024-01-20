d281072bd4c2f5715c4e4b03e7e7483c9c91aca3
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2024-01-19 20:50:03 -0800

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -6,6 +6,7 @@ const authBaseUrl =
     : "http://localhost:3001";
 
 export const registerUser = ({ email, password, name, avatar }) => {
+  console.log("Registering user:", { email, name, avatar }); // Log input data
   return fetch(`${authBaseUrl}/signup`, {
     method: "POST",
     headers: {
@@ -13,10 +14,19 @@ export const registerUser = ({ email, password, name, avatar }) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password, name, avatar }),
-  }).then(checkResponse);
+  })
+    .then((res) => {
+      console.log("Response from registerUser:", res);
+      return checkResponse(res);
+    })
+    .then((data) => {
+      console.log("Data from registerUser:", data);
+      return data;
+    });
 };
 
 export const loginUser = ({ email, password }) => {
+  console.log("Logging in user:", { email }); // Log input data
   return fetch(`${authBaseUrl}/signin`, {
     method: "POST",
     headers: {
@@ -24,20 +34,38 @@ export const loginUser = ({ email, password }) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
-  }).then(checkResponse);
+  })
+    .then((res) => {
+      console.log("Response from loginUser:", res);
+      return checkResponse(res);
+    })
+    .then((data) => {
+      console.log("Data from loginUser:", data);
+      return data;
+    });
 };
 
 export const getUserInfo = (token) => {
+  console.log("Fetching user info with token:", token); // Log input data
   return fetch(`${authBaseUrl}/users/me`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-  }).then(checkResponse);
+  })
+    .then((res) => {
+      console.log("Response from getUserInfo:", res);
+      return checkResponse(res);
+    })
+    .then((data) => {
+      console.log("Data from getUserInfo:", data);
+      return data;
+    });
 };
 
 export const editProfile = (name, avatar, token) => {
+  console.log("Editing user profile:", { name, avatar, token }); // Log input data
   return fetch(`${authBaseUrl}/users/me`, {
     method: "PATCH",
     headers: {
@@ -45,5 +73,13 @@ export const editProfile = (name, avatar, token) => {
       Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({ name, avatar }),
-  }).then(checkResponse);
+  })
+    .then((res) => {
+      console.log("Response from editProfile:", res);
+      return checkResponse(res);
+    })
+    .then((data) => {
+      console.log("Data from editProfile:", data);
+      return data;
+    });
 };
